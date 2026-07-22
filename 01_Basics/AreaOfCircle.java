84ac0bfaca6b211bc0bade9dfbcb67aed9e55c00
pavani-desaboyina <pavanidesaboina90@gmail.com>
2026-07-22 23:05:16 +0530

    Add AreaOfCircle Java program

---
+++ 01_Basics/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter radius:");
+        int r = sc.nextInt();
+        double area = 3.14 * r * r;
+        System.out.println(area);
+
+    }
+}
