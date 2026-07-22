e7ab268f216ae7855d4e74a4d0a798abaf3d8edb
pavani-desaboyina <pavanidesaboina90@gmail.com>
2026-07-22 22:34:36 +0530

    Add SwapTwoNumbers Java program

---
+++ 01_Basics/SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int temp = a;
+         a = b;
+         b = temp;
+        System.out.println("a: " + a + "b: "+b);
+        
+    }
+}
